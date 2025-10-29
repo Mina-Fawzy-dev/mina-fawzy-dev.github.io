38474bc303a089925d74758d15d85e08bdeeb952
Mina Fawzy <204509993+Mina-Fawzy-dev@users.noreply.github.com>
2025-10-30 01:29:29 +0300

    Refactor service worker caching logic

--- design-hub/sw.js
+++ design-hub/sw.js
@@ -1,25 +1,23 @@
-// sw.js
+// design-hub/sw.js
 const CACHE = 'designhub-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/css/main.css',
-  '/js/ui.js',
-  '/js/data.js',
-  '/js/mindmap.js',
-  '/js/pwa.js',
-  '/assets/logo.svg',
-  '/manifest.json'
-];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE).then(cache => cache.addAll([
+      '/',
+      '/index.html',
+      '/manifest.json',
+      '/assets/logo.svg',
+      '/js/ui.js',
+      '/js/data.js',
+      '/js/mindmap.js',
+      '/js/pwa.js'
+    ]))
   );
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then(resp => resp || fetch(e.request))
   );
 });
